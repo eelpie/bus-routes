df428807680c4f79c19966c8dba21d2a5fcc7565
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-02-02 13:58:39 +0000

    Use spring scheduled annotation to reload routes information once a week.

--- src/main/java/uk/co/eelpieconsulting/busroutes/timers/ImportStopsTask.java
+++ src/main/java/uk/co/eelpieconsulting/busroutes/timers/ImportStopsTask.java
@@ -5,29 +5,38 @@ import java.io.IOException;
 
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrServerException;
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
 
 import uk.co.eelpieconsulting.busroutes.parsing.RouteFileFinderService;
 import uk.co.eelpieconsulting.busroutes.parsing.RouteImportService;
 
-@Controller
+@Service
 public class ImportStopsTask {
 	
 	private static Logger log = Logger.getLogger(ImportStopsTask.class);
 	
-	private final RouteImportService routeImportService;
+	private RouteImportService routeImportService;
 	private RouteFileFinderService routeFileFinderService;
 	
+	public ImportStopsTask() {
+	}
+	
+	@Autowired
 	public ImportStopsTask(RouteImportService routeImportService, RouteFileFinderService routeFileFinderService) {
 		this.routeImportService = routeImportService;
 		this.routeFileFinderService = routeFileFinderService;
 	}
 	
+	@Scheduled(cron="0 4 * * * SAT")
 	public void importRoutes() throws SolrServerException, IOException {
+		log.info("Starting route import");
 		File routesFile = routeFileFinderService.findRoutesFile();
 		if (isUpdate(routesFile)) {
 			log.info("Routes file is an update; importing: " + routesFile.getAbsolutePath());
 			routeImportService.importRoutes(routesFile);
+			log.info("Finished route import");
 		} else {
 			log.info("Routes file is the same as previously imported; not importing: " + routesFile.getAbsolutePath());
 		}
