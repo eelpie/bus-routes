2797cf57634035013219c0d5f8ac3a483b7717ed
Tony McCrae <tony@eelpieconsulting.co.uk>
2013-08-24 17:58:19 +1200

    Seems to help ensure that index is created.

--- src/main/java/uk/co/eelpieconsulting/busroutes/daos/DataSourceFactory.java
+++ src/main/java/uk/co/eelpieconsulting/busroutes/daos/DataSourceFactory.java
@@ -5,6 +5,8 @@ import java.net.UnknownHostException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import uk.co.eelpieconsulting.busroutes.model.RouteStop;
+
 import com.google.code.morphia.Datastore;
 import com.google.code.morphia.Morphia;
 import com.mongodb.Mongo;
@@ -32,6 +34,7 @@ public class DataSourceFactory {
 		
 		Mongo m = new Mongo(mongoHost);
 		final Datastore dataStore = morphia.createDatastore(m, mongoDatabase);
+		morphia.map(RouteStop.class);
 		dataStore.ensureIndexes();
 		return dataStore;
 	}
