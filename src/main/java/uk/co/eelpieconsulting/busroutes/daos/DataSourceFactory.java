d9fb4d07925f2d905116a4b76a355701f94bb4bf
Tony McCrae <tony@eelpieconsulting.co.uk>
2016-07-05 12:12:12 +0100

    Enable Mongo SSL.

--- src/main/java/uk/co/eelpieconsulting/busroutes/daos/DataSourceFactory.java
+++ src/main/java/uk/co/eelpieconsulting/busroutes/daos/DataSourceFactory.java
@@ -3,6 +3,7 @@ package uk.co.eelpieconsulting.busroutes.daos;
 import java.net.UnknownHostException;
 import java.util.List;
 
+import com.mongodb.*;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,10 +12,6 @@ import org.springframework.stereotype.Component;
 import uk.co.eelpieconsulting.busroutes.model.RouteStop;
 
 import com.google.common.collect.Lists;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
-import com.mongodb.MongoException;
-import com.mongodb.ServerAddress;
 
 @Component
 public class DataSourceFactory {
@@ -30,7 +27,10 @@ public class DataSourceFactory {
     
     @Value("#{busRoutes['mongoPassword']}")
     private String mongoPassword;
-    
+
+    @Value("#{busRoutes['mongoSSL']}")
+    private Boolean mongoSSL;
+
 	public DataSourceFactory() {
 	}
 
@@ -44,8 +44,10 @@ public class DataSourceFactory {
 		
         MongoCredential credential = MongoCredential.createCredential(mongoUser, mongoDatabase, mongoPassword.toCharArray());
         List credentials = Lists.newArrayList(credential);
+
+        MongoClientOptions mongoClientOptions = MongoClientOptions.builder().sslEnabled(mongoSSL).build();
 		
-        MongoClient m = new MongoClient(new ServerAddress(mongoHost), credentials);
+        MongoClient m = new MongoClient(new ServerAddress(mongoHost), credentials, mongoClientOptions);
         final Datastore dataStore =  morphia.createDatastore(m, mongoDatabase);        
 		morphia.map(RouteStop.class);
 		dataStore.ensureIndexes();
